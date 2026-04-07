85e7ce6abf9bea80cf0c549c116bbbaa7a0081c4
Unna Mäntyniemi <unna.mantyniemi@helsinki.fi>
2026-04-07 20:31:43 +0300

    Fix pylints suggestions

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -6,11 +6,10 @@ class UserRepository:
 
     def find_all(self):
         return self._users
-    
+
     def find_by_username(self, username: str):
         return self._users.get(username)
 
     def create(self, user: User):
         self._users[user.username] = user
         return user
-    
--- src/services/user_service.py
+++ src/services/user_service.py
@@ -10,43 +10,42 @@ class UserService:
 
     def _current_user(self):
         return self._current_user
-    
+
     def logout(self):
         self._current_user = None
 
     def register(self, username: str, password: str):
         if len(username) <= 3:
             raise ValueError("Käyttäjätunnuksen tulee olla yli 3 merkkiä pitkä")
-        
+
         if self._user_repository.find_by_username(username):
             raise ValueError("Käyttäjätunnus on jo käytössä")
-        
+
         self._validate_password(password)
 
         user = User(username, self._hash_password(password))
         self._user_repository.create(user)
         return user
-    
+
     def login(self, username: str, password: str):
         user = self._user_repository.find_by_username(username)
         if not user:
             raise ValueError("Käyttäjätunnus on väärä tai sitä ei ole")
-        
         if user.password != self._hash_password(password):
             raise ValueError("Väärä salasana")
-        
+
         self._current_user = user
         return user
-    
+
     def _validate_password(self, password: str):
         if len(password) <= 8:
             raise ValueError("Salasanan tulee olla yli 8 merkkiä pitkä")
-        
+
         if not re.search(r"\d", password):
             raise ValueError("Salasanan täytyy sisältää ainakin yksi numero")
-        
+
         if not re.search(r"[^a-zA-Z0-9]", password):
             raise ValueError("Salasanan täytyy sisältää ainakin yksi erikoismerkki.")
-        
+
     def _hash_password(self, password: str):
         return hashlib.sha256(password.encode("utf-8")).hexdigest()
